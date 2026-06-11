1a34e0af7969adf1ccda2f6dc71ee9dc7b96308c
Suchith434 <kssuchith2@gmail.com>
2026-06-11 19:13:44 +0530

    Add files via upload

---
+++ reg_file.v
@@ -0,0 +1,13 @@
+module reg_file #(parameter DATA_width=4, ADDR_width=2)(
+    input clk,input we, input[ADDR_width-1:0] w_addr1, input [DATA_width-1:0] w_data, 
+    input[ADDR_width-1:0] r_addr1, output[DATA_width-1:0] r_data1, 
+    input[ADDR_width-1:0] r_addr2, output[DATA_width-1:0] r_data2);
+    reg [DATA_width-1:0] memory_array [0:(1<<ADDR_width)-1]; //builds an array of 4 distinct registers (numbered Address 0 through Address 3), and each one is exactly 4 bits wide.
+    assign r_data1=memory_array[r_addr1];
+    assign r_data2=memory_array[r_addr2];
+    always @(posedge clk) begin
+        if(we) begin
+            memory_array[w_addr1] <= w_data;
+        end
+    end
+endmodule   
